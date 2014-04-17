9b4099c1fd00eed8e567e9031253cd9310f0047e
Lakshmisha Bhat <lbhat@Lakshmishas-MacBook-Air.local>
2014-04-17 17:10:08 -0400

    set no. of folds to 0 for LogitBoost

--- src/jvm/ml/bolt/ml/state/weka/utils/WekaUtils.java
+++ src/jvm/ml/bolt/ml/state/weka/utils/WekaUtils.java
@@ -43,7 +43,7 @@ public class WekaUtils {
         for (int i = 1; i <= noOfClusters; i++)
             clusters.add(MessageFormat.format("class-{0}", String.valueOf(i)));
         Attribute cluster = new Attribute("classes", clusters);
-         // last element in a FEATURE VECTOR is the category
+        // last element in a FEATURE VECTOR is the category
         attributeInfo.add(cluster);
 
         System.err.println("DEBUG: no. of attributes = " + attributeInfo.size());
@@ -65,68 +65,84 @@ public class WekaUtils {
         return attributeInfo;
     }
 
-    public static Classifier makeClassifier(String wekaClassifier){
-        switch(WekaClassificationAlgorithms.valueOf(wekaClassifier)){
-            case decisionTree: return new J48();
-            case svm: return new SMO();
-            case logisticRegression: return new SimpleLogistic();
-            case randomForest: return new RandomForest();
-            case decisionStump: return new DecisionStump();
-            case boosting: return new LogitBoost();
-            case perceptron: return new MultilayerPerceptron();
-            default: return new SMO();
+    public static Classifier makeClassifier(String wekaClassifier) {
+        switch (WekaClassificationAlgorithms.valueOf(wekaClassifier)) {
+            case decisionTree:
+                return new J48();
+            case svm:
+                return new SMO();
+            case logisticRegression:
+                return new SimpleLogistic();
+            case randomForest:
+                return new RandomForest();
+            case decisionStump:
+                return new DecisionStump();
+            case boosting:
+                LogitBoost boost = new LogitBoost();
+                boost.setNumFolds(0);
+                return boost;
+            case perceptron:
+                return new MultilayerPerceptron();
+            default:
+                return new SMO();
         }
     }
 
-    public static Classifier makeOnlineClassifier(String wekaClassifier){
-        switch (WekaOnlineClassificationAlgorithms.valueOf(wekaClassifier)){
-            case naiveBayes: return new NaiveBayesUpdateable();
-            case locallyWeightedLearner: return new LWL();
-            case nearestNeighbors: return new IBk();
-            case onlineDecisionTree: return new HoeffdingTree();
-            case stochasticGradientDescent: return new SGD();
-            default: return new NaiveBayesUpdateable();
+    public static Classifier makeOnlineClassifier(String wekaClassifier) {
+        switch (WekaOnlineClassificationAlgorithms.valueOf(wekaClassifier)) {
+            case naiveBayes:
+                return new NaiveBayesUpdateable();
+            case locallyWeightedLearner:
+                return new LWL();
+            case nearestNeighbors:
+                return new IBk();
+            case onlineDecisionTree:
+                return new HoeffdingTree();
+            case stochasticGradientDescent:
+                return new SGD();
+            default:
+                return new NaiveBayesUpdateable();
         }
     }
 
     public static Clusterer makeClusterer(String wekaClassifier, int numClusters) throws Exception {
-        try{
-        switch(WekaClusterers.valueOf(wekaClassifier)){
-            case kmeans:
-                SimpleKMeans kmeans = new SimpleKMeans();
-                kmeans.setNumClusters(numClusters);
-                return kmeans;
-            case densityBased:
-                MakeDensityBasedClusterer clusterer = new MakeDensityBasedClusterer();
-                clusterer.setNumClusters(numClusters);
-                return clusterer;
-            case farthestFirst:
-                FarthestFirst ff = new FarthestFirst();
-                ff.setNumClusters(numClusters);
-                return ff;
-            case hierarchicalClusterer:
-                HierarchicalClusterer hc = new HierarchicalClusterer();
-                hc.setNumClusters(numClusters);
-                return hc;
-            case em:
-                EM em = new EM();
-                em.setMaxIterations(10);
-                em.setMaximumNumberOfClusters(numClusters);
-                em.setNumClusters(numClusters);
-                return em;
-            case filteredClusterer:
-                kmeans = new SimpleKMeans();
-                kmeans.setNumClusters(numClusters);
-                FilteredClusterer fc = new FilteredClusterer();
-                fc.setFilter(new AllFilter());
-                fc.setClusterer(kmeans);
-                return fc;
-            default:
-                kmeans = new SimpleKMeans();
-                kmeans.setNumClusters(numClusters);
-                return kmeans;
-        }
-        }catch (Exception e){
+        try {
+            switch (WekaClusterers.valueOf(wekaClassifier)) {
+                case kmeans:
+                    SimpleKMeans kmeans = new SimpleKMeans();
+                    kmeans.setNumClusters(numClusters);
+                    return kmeans;
+                case densityBased:
+                    MakeDensityBasedClusterer clusterer = new MakeDensityBasedClusterer();
+                    clusterer.setNumClusters(numClusters);
+                    return clusterer;
+                case farthestFirst:
+                    FarthestFirst ff = new FarthestFirst();
+                    ff.setNumClusters(numClusters);
+                    return ff;
+                case hierarchicalClusterer:
+                    HierarchicalClusterer hc = new HierarchicalClusterer();
+                    hc.setNumClusters(numClusters);
+                    return hc;
+                case em:
+                    EM em = new EM();
+                    em.setMaxIterations(10);
+                    em.setMaximumNumberOfClusters(numClusters);
+                    em.setNumClusters(numClusters);
+                    return em;
+                case filteredClusterer:
+                    kmeans = new SimpleKMeans();
+                    kmeans.setNumClusters(numClusters);
+                    FilteredClusterer fc = new FilteredClusterer();
+                    fc.setFilter(new AllFilter());
+                    fc.setClusterer(kmeans);
+                    return fc;
+                default:
+                    kmeans = new SimpleKMeans();
+                    kmeans.setNumClusters(numClusters);
+                    return kmeans;
+            }
+        } catch (Exception e) {
             throw new Exception("Could not make Clusterer", e);
         }
     }
